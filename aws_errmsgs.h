248d2d625fae1ab5f366c0c5d6a082f652d68a6d
Micah Baker <Micah.Baker@improving.com>
2025-04-08 14:17:36 -0700

    indentation change

--- aws_errmsgs.h
+++ aws_errmsgs.h
@@ -16,10 +16,10 @@
 #define __AWS_ERRMSGS_H__
 
 /* AWSpsqlODBC-specific error messages */
-#define ERRMSG_SECRETS_NOT_RETRIEVED			"Unable to retrieve credentials from Secrets Manager."
-#define ERRMSG_IAM_AUTH_FAILED				"Unable to authenticate using RDS DB IAM."
-#define ERRMSG_LIMITLESS_COULD_NOT_ALLOCATE		"There was a memory allocation error while attempting to use Limitless feature."
-#define ERRMSG_LIMITLESS_CONNECTION_NOT_ESTABLISHED	"Connection was not established while attempting to use Limitless feature."
-#define ERRMSG_LIMITLESS_NOT_LIMITLESS_CLUSTER		"Attempted to use Limitless feature on a non-Limitless cluster. Connection not established. Please disable the Limitless feature or connect to a Limitless-Supported Cluster."
+#define ERRMSG_SECRETS_NOT_RETRIEVED                "Unable to retrieve credentials from Secrets Manager."
+#define ERRMSG_IAM_AUTH_FAILED                      "Unable to authenticate using RDS DB IAM."
+#define ERRMSG_LIMITLESS_COULD_NOT_ALLOCATE         "There was a memory allocation error while attempting to use Limitless feature."
+#define ERRMSG_LIMITLESS_CONNECTION_NOT_ESTABLISHED "Connection was not established while attempting to use Limitless feature."
+#define ERRMSG_LIMITLESS_NOT_LIMITLESS_CLUSTER      "Attempted to use Limitless feature on a non-Limitless cluster. Connection not established. Please disable the Limitless feature or connect to a Limitless-Supported Cluster."
 
 #endif
